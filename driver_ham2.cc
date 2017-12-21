c473706f04f7858f38c3245ef88451a265e2b1db
Geremia Massarelli <geemassa@gmail.com>
2017-12-21 13:41:50 -0500

    In driver_ham2.cc, a class was defined to declare, allocate, and initialize arrays for the parameter space and order parameters. New bugs detected.
    When an instance is declared, the arrays are declared, allocated, and initialized. They are the coordinate variables in the parameter space (only U
    for now, though we will add t2) and the other variables, which are the order parameters (their converged values are to be saved here).
    
    Newly noticed runtime bug: at the very end of the execution (after all the destructors have been called), we get "Abort trap: 6". This may be related
    to reading arrays past the last value. Upon checking again, it seems this wasn't happening with driver_ham1.

--- driver_ham2.cc
+++ driver_ham2.cc
@@ -39,6 +39,52 @@ const double rho = 1.; // Average electron density (FIXED)
 const int U_pts = 24; const double U_bounds [2] = {0., 7.}; // Hubbard interaction
 const double M_startval = 0.1; // Choose a starting value
 
+// Class that defines the parameter space for this Hamiltonian
+class pspace_t
+{
+private:
+    // Private copy constructor (prohibits copy creation)
+    pspace_t(const pspace_t&);
+    // Private assignment operator (prohibits assignment)
+    const pspace_t& operator=(const pspace_t&);
+    
+    // Hubbard interaction
+    const int U_pts;
+    const double*const U_bounds; // Two-component array
+public:
+    double*const U_grid; // U coordinate variable
+    
+    double*const M_grid; // M variable
+    
+    // Constructor declaration
+    pspace_t(const int U_pts_, const double*const U_bounds_)
+        :U_pts(U_pts_), U_bounds(U_bounds_), U_grid(new double [U_pts_]),
+        M_grid(new double [U_pts_])
+    {
+        /* The initialization list initialized the parameters and allocates memory for 
+        the arrays. */
+        
+        // Coordinate variables are initialized
+        const bool endpoint = true; // Include endpoint (not an important choice)
+        LinInitArray(U_bounds[0], U_bounds[1], U_pts, U_grid, endpoint);
+        
+        // The other parameters (the order parameters) span the coordinate space
+        ValInitArray(U_pts, M_grid); // Initialize to zero (for now)
+        
+        std::cout << "pspace_t instance created." << std::endl;
+    }
+    // Destructor declaration
+    ~pspace_t() {
+        delete [] U_grid;
+        delete [] M_grid;
+        std::cout << "pspace_t instance deleted." << std::endl;
+    }
+    
+    void SaveData() {
+        
+    }
+};
+
 void Dispersion(const double kx, const double ky, const double t2_, 
                 std::complex<double>*const h)
 {
@@ -105,14 +151,8 @@ int main(int argc, char* argv[])
     std::complex<double>*const*const evecs = Alloc2D_z(bands_num, bands_num);
     ValInitArray(bands_num*bands_num, &(evecs[0][0])); // Initialize to zero
     
-    // Define the parameter arrays
-    double*const U_grid = new double [U_pts]; // Allocate memory
-    const bool endpoint = true; // Include endpoint (not an important choice)
-    LinInitArray(U_bounds[0], U_bounds[1], U_pts, U_grid, endpoint); // Initialize
-    
-    // Define the order parameter array, which spans the parameter space
-    double*const M_grid = new double [U_pts]; // Allocate memory
-    ValInitArray(U_pts, M_grid, M_startval); // Initialize to the starting value
+    pspace_t pspace(U_pts, U_bounds); // Declare object of type pspace (parameter space)
+    ValInitArray(U_pts, pspace.M_grid, M_startval); // Initialize to the starting value
     
     // Choose a tolerance for the equality of M and Mprime and print it.
     const double tol = 1.e-6;
@@ -124,7 +164,7 @@ int main(int argc, char* argv[])
         double M =      M_startval;
         double Mprime = M_startval;
         
-        std::cout << "U = " << U_grid[h] << std::endl; // Print current value of U
+        std::cout << "U = " << pspace.U_grid[h] << std::endl; // Print current value of U
         
         do // Iterate until self-consistency is achieved
         {
@@ -135,8 +175,8 @@ int main(int argc, char* argv[])
             for (int i=0; i<kx_pts; ++i)
             for (int j=0; j<ky_pts; ++j)
             {
-                Evaluate_ham(kspace.kx_grid[i], kspace.ky_grid[j], t2, rho, U_grid[h], M, 
-                             ham_array);
+                Evaluate_ham(kspace.kx_grid[i], kspace.ky_grid[j], t2, rho, 
+                             pspace.U_grid[h], M, ham_array);
                 simple_zheev(bands_num, &(ham_array[0][0]), &(kspace.energies[i][j][0]));
             }
             
@@ -153,8 +193,8 @@ int main(int argc, char* argv[])
             for (int i=0; i<kx_pts; ++i)
             for (int j=0; j<ky_pts; ++j)
             {
-                Evaluate_ham(kspace.kx_grid[i], kspace.ky_grid[j], t2, rho, U_grid[h], M, 
-                             ham_array);
+                Evaluate_ham(kspace.kx_grid[i], kspace.ky_grid[j], t2, rho, 
+                             pspace.U_grid[h], M, ham_array);
                 simple_zheev(bands_num, &(ham_array[0][0]), &(kspace.energies[i][j][0]), 
                              true, &(evecs[0][0]));
                 accumulator += Evaluate_M_term(mu, &(kspace.energies[i][j][0]), evecs);
@@ -165,17 +205,17 @@ int main(int argc, char* argv[])
             std::cout << "abs(Mprime-M) = " << std::abs(Mprime-M) << std::endl;
         } while (std::abs(Mprime-M) > tol);
         
-        // We save the converged M value to the array M_grid.
-        M_grid[h] = Mprime;
+        // We save the converged M value to the array pspace.M_grid.
+        pspace.M_grid[h] = Mprime;
         std::cout << std::endl;
     }
     
     
     // Print out M array
     std::cout << std::endl;
-    std::cout << "M_grid = " << std::endl;
+    std::cout << "pspace.M_grid = " << std::endl;
     for (int h=0; h<U_pts; ++h)
-        std::cout << M_grid[h] << " ";
+        std::cout << pspace.M_grid[h] << " ";
     std::cout << std::endl;
     
     
@@ -201,16 +241,14 @@ int main(int argc, char* argv[])
     newDS_t newDS(GlobalAttr, dims_num_, dim_names, dim_lengths,
                   vars_num_, var_names, path);
     
-    const double*const coord_vars [dims_num_] = {U_grid};
+    const double*const coord_vars [dims_num_] = {pspace.U_grid};
     newDS.WriteCoordVars(coord_vars);
     
-    const double*const vars [vars_num_] = {M_grid};
+    const double*const vars [vars_num_] = {pspace.M_grid};
     newDS.WriteVars(vars);
     
     
     // Deallocate memory
-    delete [] M_grid;
-    delete [] U_grid;
     Dealloc2D(ham_array);
     Dealloc2D(evecs);
     return 0;
