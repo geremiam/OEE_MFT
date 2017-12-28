cd34daee706a00103a1d3a79cb4747c8ec8ec8f2
Geremia Massarelli <geemassa@gmail.com>
2017-12-27 19:04:12 -0500

    Python driver and modules added for data plotting.
    The module nc_IO.py contains a routine for reading a NetCDF; specifically, the same kind of simple NetCDF file created in this project -- one with n
    dimensions, each with a coordinate variable, and m other variables spanning all the dimensions. The module plot_routines.py contains routines for
    creating a 2D colorplot (one uses imshow; the other, still not finalized, uses pcolormesh). The file data_anal.py is a driver that calls the other
    modules to load the data and plot it.

---
+++ data/plot_routines.py
@@ -0,0 +1,44 @@
+# plot_routines.py
+""" Various routines for plotting specific data formats. """
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def ColorPlot1(row, col, z, AxLabels=["", ""], clim=[]):
+    """ Makes a colorplot of the 2D array z, with the row variable as the vertical axis 
+    and the column variable as the horizontal axis. 'row' and 'col' are the coordinate 
+    variables for the array 'z'. The entries of 'AxLabels' are the labels for the rows 
+    and the columns, respectively. """
+    
+    fig, axes = plt.subplots()
+    
+    extent = [col[0], col[-1], 
+              row[0], row[-1]] # Determines the ranges of the axes
+    
+    if (AxLabels != []):
+        axes.set_ylabel(AxLabels[0])
+        axes.set_xlabel(AxLabels[1])
+    
+    cs = axes.imshow(z, extent=extent, interpolation="nearest", origin="lower", 
+                     aspect='auto')
+    
+    if (clim != []): # Sets colorbar limits
+        cs.set_clim(clim)
+    cbar = fig.colorbar(cs, ax=axes)
+    #cbar.ax.tick_params(labelsize=FontSize)
+    
+    plt.show()
+
+def ColorPlot2(row, col, z, AxLabels=["", ""], clim=[]):
+    """ In development... This is similar to the previous routine, but uses pcolormesh 
+    instead of imshow. """
+    
+    fig, axes = plt.subplots()
+    
+    if (AxLabels != []):
+        axes.set_xlabel(AxLabels[1])
+        axes.set_ylabel(AxLabels[0])
+    
+    axes.pcolormesh(z)
+    
+    plt.show()
