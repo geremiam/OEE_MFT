6177f2255ede1caabed9748ae63b724aa006e96e
Geremia Massarelli <geemassa@gmail.com>
2018-11-20 20:00:20 -0500

    Functionalities were added to the module 'nc_IO'. These were tested in the testing suite and seem to work as expected.
    A workaround was used to allow for complex variables. Any number of coordinate variables are allowed per dimension. Variables for the energy and the
    number of iteration loops are present by default.

--- nc_IO.h
+++ nc_IO.h
@@ -3,6 +3,10 @@
 #ifndef NC_IO_H
 #define NC_IO_H
 
+#include <netcdf.h> // For NetCDF interface
+
+void ErrorHandler(const int status);
+
 class newDS_t
 {
 private:
@@ -11,25 +15,36 @@ private:
     // Private assignment operator (prohibits assignment)
     const newDS_t& operator=(const newDS_t&);
     
-    const int dims_num;
-    const int vars_num;
+    const size_t dims_num_;
+    const size_t vars_num_;
     
-    int ncid=-99;
-    int*const dimid;
-    int*const coord_varid;
-    int*const varid;
+    int*const coord_varid_; // Array of IDs for coord vars (may not be used in whole or in part)
+    int*const varid_; // Array of IDs for user-defined variables
+    int varid_energy_=-99; // ID for energy variable
+    int varid_loops_=-99; // ID for numloops variable
 public:
-    // Constructor declaration
-    newDS_t(const std::string GlobalAttr, 
-      const int dims_num_, const std::string*const dim_names, const int*const dim_lengths,
-      const int vars_num_, const std::string*const var_names, const std::string path="");
-    // Destructor declaration
-    ~newDS_t();
+    int ncid_=-99; // ID for dataset
+    int*const dimid_; // Array of IDs for dimensions.
+    /* The last one (after user-added dims) is complex dim, as has to be the case because 
+    complex numbers are stored with real and imaginary parts in consecutive addresses in 
+    memory. */
     
-    // Write coordinate variables
-    void WriteCoordVars(const double*const*const coord_vars);
-    // Write variables
-    void WriteVars(const double*const*const vars);
+    // Constructor declaration
+    newDS_t(const size_t dims_num, const std::string*const dim_names, const size_t*const dim_lengths,
+            const size_t vars_num, const std::string*const var_names, const bool*const var_complex,
+            const std::string GlobalAttr, const std::string path="");
+    ~newDS_t(); // Destructor declaration
+    
+    void DefCoordVar(const int dimindex, const std::string name); // Define a coord variable
+    int DefCoordVar_custom(const int dimindex, const std::string name); // Define an individual coord vari whose ID the user keeps track of.
+    
+    void EndDef(); // Must be called to exit define mode
+    
+    void WriteCoordVar(const int dimindex, const double*const coord_var); // Write a coordinate variable
+    void WriteCoordVar_custom(const int coord_varid, const double*const coord_var); // Write a coord var whose ID the user keeps track of.
+    void WriteVars(const double*const*const vars); // Write variables
+    void WriteEnergy(const double*const energy); // Write energy variable
+    void WriteLoops(const int*const loops); // Write numloops variable
 };
 
 #endif
