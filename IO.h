07c2d449e620df5a88eac5771fb9c4fb52034524
Geremia Massarelli <geemassa@gmail.com>
2018-11-20 19:56:01 -0500

    In module 'IO', printing of int arrays was added.

--- IO.h
+++ IO.h
@@ -3,6 +3,8 @@
 #ifndef IO_H
 #define IO_H
 
+void PrintMatrix(const int num_rows, const int num_cols, const int*const mat, std::ostream& out);
+void PrintMatrix(const int num_rows, const int num_cols, const int*const*const mat, std::ostream& out);
 void PrintMatrix(const int num_rows, const int num_cols, const double*const mat, std::ostream& out);
 void PrintMatrix(const int num_rows, const int num_cols, const double*const*const mat, std::ostream& out);
 void PrintMatrix(const int num_rows, const int num_cols, const std::complex<double>*const mat, std::ostream& out);
